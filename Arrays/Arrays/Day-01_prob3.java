c9a8a9c59c007397f5a5c6ed4bd786d05fd554b2
M4-Mihir <mihirpatel162006@gmail.com>
2026-01-11 19:25:48 +0530

    solution for running sum of 1D array

---
+++ Arrays/Arrays/Day-01_prob3.java
@@ -0,0 +1,16 @@
+//1480. Running Sum of 1d Array in Leetcode
+
+
+class Solution {
+    public int[] runningSum(int[] nums) {
+
+        int sum = 0;
+
+        for(int i = 0; i<nums.length; i++){
+            sum = sum + nums[i];
+            nums[i] = sum;
+        }
+        return nums;
+        
+    }
+}
